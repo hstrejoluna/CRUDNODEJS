5d4b947902eb9e9a003773694cf0a0eee015881a
Héctor Sebastián Trejo Luna <trejolunatutoriales@gmail.com>
2021-01-20 12:49:17 -0500

    mysql configurado y modulo exportado

--- database/db.js
+++ database/db.js
@@ -2,7 +2,17 @@ const mysql = require('mysql');
 
 const conexion = mysql.createConnection({
     host: 'hstrejoluna-pc', // "LOCALHOST"
-    user: 'mysql',
+    user: 'root',
     password: 'osoregordeteA1!',
     database: 'crud_nodejs_db'
+});
+
+conexion.connect((error)=>{
+    if(error){
+        console.error("El error de conexión es: "+error);
+        return
+    }
+    console.log('¡Conectado a la BD MySQL!');
 })
+
+module.exports = conexion;
