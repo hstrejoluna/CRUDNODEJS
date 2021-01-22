9aa1256df2748f180200bb3e43e4d453087a3008
Héctor Sebastián Trejo Luna <trejolunatutoriales@gmail.com>
2021-01-22 10:40:49 -0500

    const de database declarada

--- router.js
+++ router.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const conexion = require('./database/db');
+
+/*
 router.get('/',(req,res)=>{
     conexion.query('SELECT * FROM users')
 })
 
+*/
 module.exports = router
